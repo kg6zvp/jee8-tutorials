3e59e211adf0000212748ba7756ac5a7ffd66ef5
Sam McCollum <smccollum@mccollum.enterprises>
2018-04-10 14:58:00 -0700

    sm - Tutorial 04 - QueryParam basics

--- src/main/java/com/airhacks/ping/boundary/PingResource.java
+++ src/main/java/com/airhacks/ping/boundary/PingResource.java
@@ -2,6 +2,7 @@ package com.airhacks.ping.boundary;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 
 /**
  *
@@ -12,8 +13,10 @@ public class PingResource {
 
 	@GET
 	@Path("say_hello")
-	public String helloWorld() {
-		return "Hello, world!";
+	public String sayHello(@QueryParam("n") String name, @QueryParam("a") int age) {
+		if(name == null)
+			return "Sorry, I don't say hello to nulls";
+		return "Hello, " + name + "! You must be " + age + " years old.";
 	}
 
 }
