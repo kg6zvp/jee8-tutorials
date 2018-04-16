ef074f81f94592dbeb6a936809f32619f494e9cb
Sam McCollum <smccollum@mccollum.enterprises>
2018-04-16 16:38:51 -0700

    sm - Tutorial 08 - @Produces(mimetype) in Jax-RS

--- src/main/java/com/airhacks/PeopleApi.java
+++ src/main/java/com/airhacks/PeopleApi.java
@@ -2,12 +2,15 @@ package com.airhacks;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  *
  * @author smccollum
  */
 @Path("people")
+@Produces(MediaType.APPLICATION_JSON)
 public class PeopleApi {
 	@GET
 	public Person getPerson(){
