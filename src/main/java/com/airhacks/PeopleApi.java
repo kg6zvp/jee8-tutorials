3882572866858dc04278d048dbdfc730e8db5a09
Sam McCollum <smccollum@mccollum.enterprises>
2018-04-16 17:32:12 -0700

    sm - Tutorial 09 - Uploading with HTTP POST

--- src/main/java/com/airhacks/PeopleApi.java
+++ src/main/java/com/airhacks/PeopleApi.java
@@ -1,6 +1,7 @@
 package com.airhacks;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -16,6 +17,12 @@ public class PeopleApi {
 	public Person getPerson(){
 		Person p = new Person("Bourne", 35);
 		System.out.println(p);
+		return p;
+	}
+
+	@POST
+	public Person postPerson(Person p){
+		System.out.println("Person received: " + p);
 		return p;
 	}
 }
